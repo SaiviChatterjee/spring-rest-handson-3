95773547e3166ad017f9fdddc8a845bebeadcf73
SaiviChatterjee <40817236+SaiviChatterjee@users.noreply.github.com>
2020-06-13 13:27:57 +0530

    Add files via upload

---
+++ spring-rest-handson-3/src/main/java/com/cognizant/springlearn/SpringLearn2Application.java
@@ -0,0 +1,16 @@
+package com.cognizant.springlearn;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringLearn2Application {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLearn2Application.class, args);
+ 	}
+
+}
